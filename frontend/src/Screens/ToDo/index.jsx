813fb497fa1634ceb4dcd48bd9f707a309d26a26
= <=>
2021-06-23 17:35:08 +0200

    Style form

--- frontend/src/Screens/ToDo/index.jsx
+++ frontend/src/Screens/ToDo/index.jsx
@@ -54,18 +54,72 @@ export default function ToDo() {
     <>
       <Header component="toDo" />
       <section className="new-task">
-        <p> AÑADIR TAREA </p>
-        <FontAwesomeIcon icon={faPlusCircle} className="new-task__icon" onClick={() => { setNewTask(!newTask); }} />
+        <button
+          className="new-task__button"
+          onClick={() => { setNewTask(!newTask); }}
+          type="button"
+        >
+          AÑADIR TAREA
+          <FontAwesomeIcon icon={faPlusCircle} className="new-task__icon" />
+        </button>
         {
           newTask
           && (
           <div className="new-task__form">
-            <p>hola</p>
+            <label htmlFor="taskName">
+              Escribe el nombre de la tarea:
+              <input
+                type="text"
+                name="taskName"
+                id="taskName"
+                placeholder="Escribe una tarea."
+                className="form__input"
+              />
+            </label>
+            <label htmlFor="priority">
+              Escribe un número que determine la prioridad:
+              <input
+                type="number"
+                name="priority"
+                id="priority"
+                placeholder="Del 1 al 5"
+                className="form__input"
+              />
+            </label>
+            <label htmlFor="class">
+              ¿A qué grupo pertenece esta tarea?
+              <input
+                type="text"
+                name="class"
+                id="class"
+                className="form__input"
+                placeholder="¿A qué grupo pertenece?"
+              />
+            </label>
+            <label htmlFor="expiration">
+              ¿Cuándo vence la tarea?
+              <input
+                type="date"
+                name="expiration"
+                id="expiration"
+                className="form__input"
+              />
+            </label>
+            <button
+              type="submit"
+              className="form__input-button"
+            >
+              ENVIAR
+
+            </button>
           </div>
           )
         }
       </section>
-      <Table component="ToDo" columns={columns} data={data} />
+      {
+       !newTask
+      && <Table component="ToDo" columns={columns} data={data} />
+     }
     </>
   );
 }
